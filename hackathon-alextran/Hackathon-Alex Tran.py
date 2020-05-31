999f2c89b8c6e2b69b2aeb9ff2aec344b361202d
alextran999 <66195531+alextran999@users.noreply.github.com>
2020-05-31 01:58:39 -0400

    Add files via upload

---
+++ hackathon-alextran/Hackathon-Alex Tran.py
@@ -0,0 +1,263 @@
+print("="*80)
+print("\t\t\tWelcome to Covid, the TRIVIA!")
+print("-"*80)
+
+print("Question 1")
+x=0
+Q1=input(" Is Corona virus or Covid-19 real:  ")
+Q1=Q1.lower()
+
+if Q1 ==" Yes ":
+    x=x+1
+    print("Correct!")
+else:
+    print("Incorrect, the correct answer is Yes : ")
+print()
+print()
+
+
+print("Question 2")
+Q2=input(" How is Covid-19 spreaded:   ")
+Q2=Q2.lower()
+if Q2 ==" Coronavirus is spreaded from an infected person ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Coronavirus is spread from an infected person through!")
+print()
+print()
+
+
+print("Question 3")
+Q3=input(" Can you get sick from people who don’t have symptoms:   ")
+Q3=Q3.lower()
+if Q3 ==" Yes ":
+    print("Correct")
+    x=x+1
+else: 
+    print("Incorrect, the right answer is Yes !")
+print()
+print()
+
+print("Question 4")
+Q4=input(" Is it safe to handle money and documents :   ")
+Q4=Q4.lower()
+if Q4 ==" No ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is No !")
+print()
+print()
+
+print("Question 5")
+Q5=input(" How do you protect yourself from Covid-19?  :  ")
+Q5=Q5.lower()
+if Q5==" Wearing a masks":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is wearing a masks ! ")
+print()
+print()
+
+print("Question 6")
+Q6=input(" What is physical distancing?:    ")
+Q6=Q6.lower()
+if Q6 ==" Staying 6 feets apart ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is staying 6 feets apart !")
+print()
+print()
+
+print("Question 7")
+Q7=input(" How can you practice social distancing ")
+Q7=Q7.lower()
+if Q7 ==" keeping about two meters (six feet) ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is keeping about two meters (six feet) !.")
+print()
+print()
+
+print("Question 8")
+Q8=input(" Is it safe to go out and shop, take public transit or go to work ")
+Q8=Q8.lower()
+if Q8 ==" No ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is No !")
+print()
+print()
+
+print("Question 9")
+Q9=input(" What is the main symptom of Covid-19 ")
+Q9=Q9.lower()
+if Q9 ==" Respiratory illnesses ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Respiratory illnesses !")
+print()
+print()
+
+print("Question 10")
+Q10=input(" What should you do if you have Covid-19  ")
+Q10=Q10.lower()
+if Q10 ==" Stay home and call a doctor ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is stay home and call a doctor !")
+print()
+print()
+
+print("Question 11")
+Q11=input(" How do you disinfect and clean you house to stay healthy  ")
+Q11=Q11.lower()
+if Q11 ==" Water and detergent.‎‎ ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Water and detergent!")
+print()
+print()
+
+print("Question 12")
+Q12=input(" What is self monitoring ")
+Q12=Q12.lower()
+if Q12 ==" Self monitoring means you should be monitoring your health and the health of your children for symptoms such as fever, cough or difficulty breathing ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is self monitoring means you should be monitoring your health and the health of your children for symptoms such as fever, cough or difficulty breathing !")
+print()
+print()
+
+print("Question 13")
+Q13=input(" What is self isolation ")
+Q13=Q13.lower()
+if Q13 ==" Self isolation is used to lower the chance of spreading infectious germs to other people":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Self isolation is used to lower the chance of spreading infectious germs to other people !")
+print()
+print()
+
+print("Question 14")
+Q14=input("  Is it bad to break self isolation ")
+Q14=Q14.lower()
+if Q14 ==" Yes":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Yes !")
+print()
+print()
+
+print("Question 15")
+Q15=input(" How is Covid 19 diagnosed ")
+Q15=Q15.lower()
+if Q2 ==" COVID-19 testing is done using samples collected by a nasopharyngeal swab (NP) or throat swab ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is COVID-19 testing is done using samples collected by a nasopharyngeal swab (NP) or throat swab !")
+print()
+print()
+
+print("Question 16")
+Q16=input(" What are antibodies ")
+Q16=Q16.lower()
+if Q16 ==" Antibodies help protect you from getting sick ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Antibodies help protect you from getting sick.!")
+print()
+print()
+
+print("Question 17")
+Q17=input(" Is there a treatment for Covid-19 ")
+Q17=Q17.lower()
+if Q17 ==" No ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is No !")
+print()
+print()
+
+print("Question 18")
+Q18=input(" Are travellers coming in from Canada being screened")
+Q18=Q18.lower()
+if Q18 ==" Yes ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Yes !")
+print()
+print()
+
+print("Question 19")
+Q19=input(" Could animal have caused Covid- 19 ")
+Q19=Q19.lower()
+if Q19 ==" Yes ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Yes !")
+print()
+print()
+
+print("Question 20")
+Q20=input(" Why does Covid-19 spread ")
+Q20=Q20.lower()
+if Q20 ==(" The virus that causes COVID-19 is thought to spread mainly from person to person"):
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is The virus that causes COVID-19 is thought to spread mainly from person to person!")
+print()
+print()
+
+print("Question 21")
+Q21=input(" What temperature kills the virus that causes Covid-19 ")
+Q21=Q21.lower()
+if Q21 ==" higher temperatures ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is higher temperature !")
+print()
+print()
+
+print("Question 22")
+Q22=input(" What kinds of tests are used to diagnose Covid-19")
+Q22=Q22.lower()
+if Q22 ==" Viral tests ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is Viral tests !")
+print()
+print()
+
+print("Question 23")
+Q23=input(" Who are at higher risks for serious illnesses from Covid-19")
+Q23=Q23.lower()
+if Q23 ==" Older People ":
+    print("Correct")
+    x=x+1
+else:
+    print("Incorrect, the right answer is COVID-19 is Older people !")
+print()
+print()
+total=(x/5)*100
+print ("\n\nCongradulations, you got",int(total),"% percent on this trivia!")
+   
